585a5f5519ffbe25e92b5e08e29e78cd87eeb235
aemir <aysun.emir@gmail.com>
2020-09-28 19:12:41 +0300

    Room Database

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/persistence/RecipeDatabase.java
@@ -0,0 +1,24 @@
+package com.codingwithmitch.foodrecipes.persistence;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.codingwithmitch.foodrecipes.models.Recipe;
+
+@Database(entities = {Recipe.class}, version = 1)
+public abstract class RecipeDatabase extends RoomDatabase {
+
+    public static final String DATABASE_NAME = "recipes_db";
+
+    private static RecipeDatabase instance;
+
+    public static RecipeDatabase getInstance(final Context context) {
+        if (instance == null) {
+            instance = Room.databaseBuilder(context, RecipeDatabase.class, DATABASE_NAME).build();
+        }
+        return instance;
+    }
+}
